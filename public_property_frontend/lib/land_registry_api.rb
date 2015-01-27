4e440b2fc43167ef6957f07c866d6023f19a4fde
Pheonnexx <pheonn@hotmail.co.uk>
2015-01-27 15:43:00 +0000

    property api consumption start

---
+++ public_property_frontend/lib/land_registry_api.rb
@@ -0,0 +1,13 @@
+class LandRegistryApi
+  def get(postcode, address_string)
+    #call the api with params postcode and address_string
+    #take that json and format it so that it looks like the thing below!
+    {
+      :address => "21 Murhill Lane",
+      :property_type => "house",
+      :price_paid_info => {:price => "200", :date => '12/12/2013'},
+      :council_info => {:name => 'devon', :url => 'www.bbc.co.uk'},
+      :coordinates => {:latitude => '100', :longitude => '100'},
+    }
+  end
+end
