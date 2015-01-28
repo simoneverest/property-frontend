1b50dc7ffa3ff87903b448d005f44b7c81d47a5c
Andrew Moore <mooreandrewlr@gmail.com>
2015-01-28 14:45:45 +0000

    Added environment.sh file

--- lib/land_registry_api.rb
+++ lib/land_registry_api.rb
@@ -2,6 +2,10 @@ class LandRegistryApi
   def get(postcode, address_string)
     #call the api with params postcode and address_string
     #take that json and format it so that it looks like the thing below!
+
+    # When you need access the property api, you should it using the following:
+    # url = ENV['PROPERTY_API'] + '/properties/.../...'
+
     {
       :address => "21 Murhill Lane",
       :property_type => "house",
