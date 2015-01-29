f6d24a5da09b28ba868077d9ba8cdd03ee7e311f
Nigel Pain <nigel.pain@digital.landregistry.gov.uk>
2015-01-29 15:07:16 +0000

    amended unit tests so they would pass, also discovered more tests around error handling so have added these as comments for review

--- spec/property_data_spec.rb
+++ spec/property_data_spec.rb
@@ -5,9 +5,15 @@ describe PropertyData do
   let(:api_client) { double(:api_client, :get => fake_json_data) }
   let(:fake_json_data) {
     {
-      "saon" => "A",
+      "paon" => "A",
+      "saon" => "B",
       "street" => "Test Street",
-      "town" => "Plymouth"
+      "town" => "Plymouth",
+      "county" => "Devon",
+      "postcode" => "PL8 2JF",
+      "property type" => "House",
+      "price paid info" => {"amount" => 3, "date" => "23rd June"},
+      "coordinates" => {"latitude" => 23.6, "longitude" => 15.4}
     }
   }
   let(:postcode) { "PL9 8TB" }
@@ -19,9 +25,24 @@ describe PropertyData do
     expect(api_client).to have_received(:get).with("/properties/PL9%208TB/testaddressstring")
   end
 
-  it "formats the address in a correct way" do
+  it "formats a full address correctly" do
     property_data = described_class.new(api_client)
     result = property_data.find(postcode, address_string)
-    expect(result[:address]).to eq("A Test Street Plymouth")
+    expect(result[:address]).to eq("A B Test Street Plymouth Devon PL8 2JF")
   end
+=begin
+  it "formats a partial address correctly" do
+    true
+  end
+=end
+=begin
+  it "returns the appropriate response when no map coordinates are found" do
+    true
+  end
+=end
+=begin
+  it "returns the appropriate response when no PPI data is found" do
+    true
+  end
+=end
 end
