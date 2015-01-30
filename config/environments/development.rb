b454c011ff7285ad6d55e4eece0d717d9fe0f972
Andrew Moore <mooreandrewlr@gmail.com>
2015-01-30 13:48:23 +0000

    Changed endpoint to be controlled via environment variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.property_title_api_url = "http://localhost:5000"
+  config.property_title_api_url = (ENV['PROPERTY_API'] || "http://localhost:8001")
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
