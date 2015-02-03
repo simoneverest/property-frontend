f6c7cddf723bc241cbca1910a36bd574e081a668
Pheonnexx <pheonn@hotmail.co.uk>
2015-02-03 09:33:54 +0000

    putting together messages when data is missing or not available

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
+
 end
