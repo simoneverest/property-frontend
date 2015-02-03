b3d27029656ab41dd86cf1713fad244ef071bfb8
Pheonnexx <pheonn@hotmail.co.uk>
2015-02-03 09:57:03 +0000

    updated controllers to deal with 404 and to remove databases in controller tests

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -7,5 +7,8 @@ class PropertiesController < ApplicationController
     api_client = ApiClient.new(Rails.application.config.property_title_api_url)
     data_repository = PropertyData.new(api_client)
     @property = data_repository.find(params[:postcode], params[:address_string])
+    unless @property
+      record_not_found
+    end 
   end
 end
--- test/controllers/properties_controller_test.rb
+++ test/controllers/properties_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class PropertiesControllerTest < ActionController::TestCase
 
   test "should get property page" do
-
+    
   end
 
   test "should return 404 if property not found" do
