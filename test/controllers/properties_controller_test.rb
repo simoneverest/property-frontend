efdbc7cfe2879f0c47bb5cd9983a3ddf9a8ee7ea
Camille Baldock <camille@camillebaldock.co.uk>
2015-02-05 13:16:01 +0000

    Add functional test for property with PPI data

--- test/controllers/properties_controller_test.rb
+++ test/controllers/properties_controller_test.rb
@@ -2,8 +2,18 @@ require 'test_helper'
 
 class PropertiesControllerTest < ActionController::TestCase
 
-  test "should get property page" do
-
+  test "should display full address, type, coordinates and price paid info for property with PPI" do
+    VCR.use_cassette('PPI data') do
+      get(:show, postcode: 'PL9%208TB', address_string: 'southernway_43')
+      assert_response :success
+      assert_template :application
+      property = assigns(:property)
+      assert_equal ["saon goes here", "paon goes here street goes here", "town goes here", "county goes here", "postcode goes here"], property[:address]
+      assert_equal "terraced", property[:property_type]
+      assert_equal "£107,500 on 30 September 2010", property[:price_paid_info]
+      assert_equal 99, property[:coordinates][:latitude]
+      assert_equal 99, property[:coordinates][:longitude]
+    end
   end
 
   test "should return 404 if property not found with 'Property not found' error" do
